475c549c848d59d21c7237baba669bf050234f84
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-12 21:52:21 +0100

    Update climate.py

--- components/innova_climate/climate.py
+++ components/innova_climate/climate.py
@@ -28,21 +28,18 @@ CONFIG_SCHEMA = (
     )
     .extend(cv.polling_component_schema("60s"))
 )
-        
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
-    
+
     # Set up temperature sensor if defined
-#    if CONF_TEMPERATURE in config:
-#        sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
-#        cg.add(var.set_temperature_sensor(sens))
+ #   if CONF_TEMPERATURE in config:
+ #       sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
+ #       cg.add(var.set_temperature_sensor(sens))
 
     # Set up humidity sensor if defined
 #    if CONF_HUMIDITY in config:
 #        sens = await sensor.new_sensor(config[CONF_HUMIDITY])
  #       cg.add(var.set_humidity_sensor(sens))
-
-
-
