599b48d1a1ec75b26edaae0c9344c9e5460a76ca
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-12 17:08:31 +0100

    Update climate.py

--- components/innova_climate/climate.py
+++ components/innova_climate/climate.py
@@ -12,12 +12,22 @@ innova_ns = cg.esphome_ns.namespace("innova")
 
 InnovaClimate = innova_ns.class_("InnovaClimate", climate.Climate, cg.PollingComponent)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(InnovaClimate),
-    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(),
-    cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(),
-}).extend(cv.polling_component_schema('60s')).extend(climate.CLIMATE_SCHEMA)
 
+#CONFIG_SCHEMA = cv.Schema({
+#    cv.GenerateID(): cv.declare_id(InnovaClimate),
+#    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(),
+#    cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(),
+#}).extend(cv.polling_component_schema('60s')).extend(climate.CLIMATE_SCHEMA)
+
+
+CONFIG_SCHEMA = (
+    climate.CLIMATE_SCHEMA(
+        {
+            cv.GenerateID(): cv.declare_id(Innova),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
         
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -25,14 +35,14 @@ async def to_code(config):
     await climate.register_climate(var, config)
     
     # Set up temperature sensor if defined
-    if CONF_TEMPERATURE in config:
-        sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
-        cg.add(var.set_temperature_sensor(sens))
+ #   if CONF_TEMPERATURE in config:
+ #       sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
+ #       cg.add(var.set_temperature_sensor(sens))
 
     # Set up humidity sensor if defined
-    if CONF_HUMIDITY in config:
-        sens = await sensor.new_sensor(config[CONF_HUMIDITY])
-        cg.add(var.set_humidity_sensor(sens))
+#    if CONF_HUMIDITY in config:
+#        sens = await sensor.new_sensor(config[CONF_HUMIDITY])
+ #       cg.add(var.set_humidity_sensor(sens))
 
 
 
