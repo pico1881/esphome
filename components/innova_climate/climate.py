3dc9171799899fb1ef7190009ffb1463779f62d6
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-12 15:16:40 +0100

    Update climate.py

--- components/innova_climate/climate.py
+++ components/innova_climate/climate.py
@@ -1,31 +1,43 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, modbus
-from esphome.const import CONF_ID, CONF_UNIT_OF_MEASUREMENT
+from esphome.components import climate
 
-UNITS = {
-    "f": "f",
-    "c": "c",
-}
+from esphome.const import (
+    CONF_ID, 
+    CONF_TEMPERATURE, 
+    CONF_HUMIDITY
+)
 
 innova_ns = cg.esphome_ns.namespace("innova")
 Innova = innova_ns.class_(
     "Innova", climate.Climate, cg.PollingComponent
 )
 
-CONFIG_SCHEMA = (
-    climate.CLIMATE_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(Innova),
-            cv.Optional(CONF_UNIT_OF_MEASUREMENT): cv.enum(UNITS),
-        }
-    )
-    .extend(cv.polling_component_schema("60s"))
-)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(Innova),
+    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(),
+    cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(),
+}).extend(cv.polling_component_schema('60s')).extend(climate.CLIMATE_SCHEMA)
 
 
 async def to_code(config):
+    # Create an instance of the CustomClimate component
     var = cg.new_Pvariable(config[CONF_ID])
+    
+    if CONF_TEMPERATURE in config:
+        conf = config[CONF_TEMPERATURE]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_temperature_sensor(sens))
+
+    if CONF_HUMIDITY in config:
+        conf = config[CONF_HUMIDITY]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_humidity_sensor(sens))
+        
+
+    # Set up the custom climate component
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
-    cg.add(var.set_unit_of_measurement(config[CONF_UNIT_OF_MEASUREMENT]))
+
+ar.set_unit_of_measurement(config[CONF_UNIT_OF_MEASUREMENT]))
