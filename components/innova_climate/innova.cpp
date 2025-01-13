50a7a9144d79d4aa10892dad36fe7043a068daa6
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-13 11:56:15 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -7,7 +7,21 @@ namespace innova {
 static const char *const TAG = "innova";
 static const uint8_t CMD_READ_REG = 0x03;
 
-void Innova::dump_config() { LOG_CLIMATE("", "Innova Climate", this); }
+void Innova::dump_config() { 
+  LOG_CLIMATE("", "Innova Climate", this); 
+  ESP_LOGCONFIG(TAG, "INNOVA:");
+  ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
+}
+
+void Innova::setup() {
+   //send(CMD_READ_REG, 0, 2);
+   this->current_temperature = 20.0;
+   this->target_temperature = 21.0;
+   this->mode = climate::CLIMATE_MODE_HEAT;
+   this->action = climate::CLIMATE_ACTION_HEATING;
+   this->fan_mode = climate::CLIMATE_FAN_LOW;
+   this->publish_state();
+}
 
 void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
   auto get_16bit = [&](int i) -> uint16_t { return (uint16_t(data[i * 2]) << 8) | uint16_t(data[i * 2 + 1]); };
@@ -32,13 +46,7 @@ void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
  }
 
 void Innova::update() {
-   //send(CMD_READ_REG, 0, 2);
-   this->current_temperature = this->current_temp_;
-   this->target_temperature = 21.0;
-   this->mode = climate::CLIMATE_MODE_HEAT;
-   this->action = climate::CLIMATE_ACTION_HEATING;
-   this->fan_mode = climate::CLIMATE_FAN_LOW;
-   this->publish_state();
+
 }
 
 }  // namespace innova
