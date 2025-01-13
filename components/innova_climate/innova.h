cbe329c5f5baeede297638965da5ec9a594df51e
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-13 09:21:03 +0100

    Update innova.h

--- components/innova_climate/innova.h
+++ components/innova_climate/innova.h
@@ -13,15 +13,8 @@ class Innova : public esphome::climate::Climate, public PollingComponent {
 
   void setup() override{}
 
-  void dump_config() override{}
-  void update() override{
-   this->current_temperature = 20.0;
-   this->target_temperature = 21.0;
-   this->mode = climate::CLIMATE_MODE_HEAT;
-   this->action = climate::CLIMATE_ACTION_HEATING;
-   this->fan_mode = climate::CLIMATE_FAN_MEDIUM;
-   this->publish_state();
-  }
+  void dump_config() override;
+  void update() override;
 
   climate::ClimateTraits traits() override {
     // Return the traits of this climate device.
