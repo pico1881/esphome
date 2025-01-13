ce9fca7ab6792b37eb3e10b6812f3f2a8bb22cab
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-13 15:55:35 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -12,6 +12,7 @@ static const uint16_t INNOVA_FAN_SPEED = 0x0F; // reg 15
 static const uint16_t INNOVA_PROGRAM = 0xC9;  // reg 201
 static const uint16_t INNOVA_SEASON = 0xE9;  // reg 233
 static const uint16_t INNOVA_SETPOINT = 0xE7; // reg 231
+static const uint16_t REGISTER[] = {INNOVA_AIR_TEMPERATURE, INNOVA_SETPOINT};
 
 void Innova::setup() {
    this->current_temperature = 20.0;
@@ -24,9 +25,23 @@ void Innova::setup() {
 
 void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
   auto get_16bit = [&](int i) -> uint16_t { return (uint16_t(data[i * 2]) << 8) | uint16_t(data[i * 2 + 1]); };
-//  this->waiting_ = false;
+  this->waiting_ = false;
   float value = (float) get_16bit(0);
-  value /= 10.0;
+   switch (this->state_) {
+     case 1:
+       value /= 10.0;
+       ESP_LOGD(TAG, "Air=%.1f", value);
+       this->current_temp_ = value;
+       break;
+     case 2:
+       value /= 10.0;
+       ESP_LOGD(TAG, "Target=%.1f", value);
+       this->target_temp_ = value;
+       break;
+   }
+   if (++this->state_ > 2)
+     this->state_ = 0;
+   
   this->current_temp_ = value;
   //this->current_temperature = value;
   //this->publish_state();
@@ -34,22 +49,23 @@ void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
 }
 
  void Innova::loop() {
-//   uint32_t now = millis();
+   uint32_t now = millis();
    // timeout after 15 seconds
-//   if (this->waiting_ && (now - this->last_send_ > 15000)) {
-//     ESP_LOGW(TAG, "timed out waiting for response");
-//     this->waiting_ = false;
-//   }
-//   if (this->waiting_ )
-//     return;
-//   this->last_send_ = now;
-//   send(CMD_READ_REG, INNOVA_AIR_TEMPERATURE, 2);
-//   this->waiting_ = true;
+   if (this->waiting_ && (now - this->last_send_ > 15000)) {
+     ESP_LOGW(TAG, "timed out waiting for response");
+     this->waiting_ = false;
+   }
+   if (this->waiting_ || (this->state_ == 0))
+     return;
+   this->last_send_ = now;
+   send(CMD_READ_REG, REGISTER[this->state_ - 1], 2);
+   this->waiting_ = true;
  }
-
 void Innova::update() {
-   send(CMD_READ_REG, 0, 2);
+   //send(CMD_READ_REG, 0, 2);
+   this->state_ = 1;
    this->current_temperature = this->current_temp_;
+   this->target_temperature = this->target_temp_;
    this->publish_state();
 }
 
