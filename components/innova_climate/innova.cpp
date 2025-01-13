46d3da0dcef836188196baef6d5237191ce6e0a2
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-13 17:57:41 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -81,8 +81,8 @@ void Innova::control(const climate::ClimateCall &call) {
       // User requested target temperature change
       //this->target_temperature = *call.get_target_temperature();
       int target = *call.get_target_temperature() * 10;
-      ESP_LOGD(TAG, "Target=%.1f", target);
-      send(CMD_WRITE_REG,INNOVA_SETPOINT,target);
+      ESP_LOGD(TAG, "Target=%x", target);
+      send(CMD_WRITE_REG,INNOVA_SETPOINT,2,target);
     }
     this->publish_state();
   }
