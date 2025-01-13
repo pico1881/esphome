b34a76178463534bc276c425ab21ed91742e63ea
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-13 11:31:05 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -12,7 +12,7 @@ void Innova::dump_config() { LOG_CLIMATE("", "Innova Climate", this); }
 void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
   auto get_16bit = [&](int i) -> uint16_t { return (uint16_t(data[i * 2]) << 8) | uint16_t(data[i * 2 + 1]); };
   float value = (float) get_16bit(0);
-  this->current_temp_->publish_state(value);
+  this->current_temp_ = value;
 }
 
 void Innova::update() {
