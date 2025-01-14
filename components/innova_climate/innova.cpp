059fb685e41bf030ddc15ead1f9109d4e0f72197
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-14 13:10:04 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -52,7 +52,7 @@ void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
        //ESP_LOGD(TAG, "Program=%.1f", value);
        this->program_ = value;   
 	climate::ClimateFanMode fmode;
-	int fstate = value;
+	auto fstate = (int)value;
         switch (fstate) {
 	  case 0: fmode = climate::CLIMATE_FAN_AUTO; break;
           case 1: fmode = climate::CLIMATE_FAN_MEDIUM; break;
