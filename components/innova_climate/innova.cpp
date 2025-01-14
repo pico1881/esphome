a2a42a77b6ad252a643e422a38bf2df1ab0ec27d
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-14 10:05:41 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -26,7 +26,10 @@ void Innova::setup() {
 
 void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
   auto get_16bit = [&](int i) -> uint16_t { return (uint16_t(data[i * 2]) << 8) | uint16_t(data[i * 2 + 1]); };
+	
   this->waiting_ = false;
+  ESP_LOGD(TAG, "Data: %s", hexencode(data).c_str());
+	
   float value = (float) get_16bit(0);
    switch (this->state_) {
      case 1:
