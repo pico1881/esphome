f650c7856087ec0f6e48f7cf6c31e4e751dea855
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-15 11:11:38 +0100

    update

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -16,150 +16,147 @@ static const uint16_t INNOVA_SETPOINT = 0xE7; // reg 231
 static const uint16_t REGISTER[] = {INNOVA_AIR_TEMPERATURE, INNOVA_SETPOINT, INNOVA_FAN_SPEED, INNOVA_PROGRAM, INNOVA_SEASON, INNOVA_WATER_TEMPERATURE};
 
 void Innova::setup() {
-   this->current_temperature = 20.0;
-   this->target_temperature = 21.0;
-   this->mode = climate::CLIMATE_MODE_HEAT;
-   this->action = climate::CLIMATE_ACTION_HEATING;
-   this->fan_mode = climate::CLIMATE_FAN_LOW;
-   this->publish_state();
+    this->current_temperature = 20.0;
+    this->target_temperature = 21.0;
+    this->mode = climate::CLIMATE_MODE_HEAT;
+    this->action = climate::CLIMATE_ACTION_HEATING;
+    this->fan_mode = climate::CLIMATE_FAN_LOW;
+    this->publish_state();
 }
 
 void Innova::on_modbus_data(const std::vector<uint8_t> &data) {
-  auto get_16bit = [&](int i) -> uint16_t { return (uint16_t(data[i * 2]) << 8) | uint16_t(data[i * 2 + 1]); };
+    auto get_16bit = [&](int i) -> uint16_t { return (uint16_t(data[i * 2]) << 8) | uint16_t(data[i * 2 + 1]); };
 	
-  this->waiting_ = false;
-  ESP_LOGD(TAG, "Data: %s", format_hex_pretty(data).c_str());
+    this->waiting_ = false;
+    ESP_LOGD(TAG, "Data: %s", format_hex_pretty(data).c_str());
 
-	//  Command response is 4 bytes echoing the write command
-	if (waiting_for_write_ack_ )  {
-		waiting_for_write_ack_ = false ; 
-		if (data.size() == 4) {
-			ESP_LOGD(TAG, "Write command succeeded");
-		} else {
-			ESP_LOGW(TAG, "Invalid data packet size (%d) while waiting for write command response", data.size());
-		}
-		return ; 
-	}
+  	//  Command response is 4 bytes echoing the write command
+  	if (waiting_for_write_ack_ )  {
+  		waiting_for_write_ack_ = false ; 
+	  	if (data.size() == 4) {
+	  		ESP_LOGD(TAG, "Write command succeeded");
+	  	} else {
+	  		ESP_LOGW(TAG, "Invalid data packet size (%d) while waiting for write command response", data.size());
+	  	}
+  		return ; 
+  	}
 	
-  float value = (float) get_16bit(0);
-  int fan_value = (int) value & 0b111;
-   switch (this->state_) {
-     case 1:
-       value /= 10.0;
-       //ESP_LOGD(TAG, "Current temperature=%.1f", value);
-       this->current_temp_ = value;
-       this->current_temperature = value;
-       break;
-     case 2:
-       value /= 10.0;
-       //ESP_LOGD(TAG, "Target temperature=%.1f", value);
-       this->target_temp_ = value;
-       this->target_temperature = value;     
-       break;
-     case 3:
-       //ESP_LOGD(TAG, "Fan speed=%.1f", value);
-       this->fan_speed_ = value;   
-       break;
-     case 4:
-       //ESP_LOGD(TAG, "Program=%.1f", value);
-       this->program_ = value;   
-	climate::ClimateFanMode fmode;
+    float value = (float) get_16bit(0);
+    int fan_value = (int) value & 0b111;
+    switch (this->state_) {
+      case 1:
+        value /= 10.0;
+        //ESP_LOGD(TAG, "Current temperature=%.1f", value);
+        this->current_temp_ = value;
+        this->current_temperature = value;
+        break;
+      case 2:
+        value /= 10.0;
+        //ESP_LOGD(TAG, "Target temperature=%.1f", value);
+        this->target_temp_ = value;
+        this->target_temperature = value;     
+        break;
+      case 3:
+        //ESP_LOGD(TAG, "Fan speed=%.1f", value);
+        this->fan_speed_ = value;   
+        break;
+      case 4:
+        //ESP_LOGD(TAG, "Program=%.1f", value);
+        this->program_ = value;   
+    	  climate::ClimateFanMode fmode;
         switch (fan_value) {
-	  case 0: fmode = climate::CLIMATE_FAN_AUTO; break;
+	        case 0: fmode = climate::CLIMATE_FAN_AUTO; break;
           case 1: fmode = climate::CLIMATE_FAN_MEDIUM; break;
           case 2: fmode = climate::CLIMATE_FAN_LOW; break;
           case 3: fmode = climate::CLIMATE_FAN_HIGH; break;
           default: fmode = climate::CLIMATE_FAN_OFF; break;
         }
-       fan_mode = fmode;     
+        fan_mode = fmode;     
        break;
-     case 5:
-       //ESP_LOGD(TAG, "Season=%.1f", value);
-       this->season_ = value;   
-       break;
-     case 6:
-       value /= 10.0;
-       //ESP_LOGD(TAG, "Water temperature=%.1f", value);
-       this->water_temp_ = value;   
-       break;
-   }
-   if (++this->state_ > 6)
-     this->state_ = 0;
-   ESP_LOGD(TAG, "Air temperature=%.1f", this->current_temp_);
-   ESP_LOGD(TAG, "Setpoint temperature=%.1f", this->target_temp_);
-   ESP_LOGD(TAG, "Fan speed=%.1f", this->fan_speed_);
-   ESP_LOGD(TAG, "Program=%.1f", this->program_);
-   ESP_LOGD(TAG, "Season=%.1f", this->season_);
-   ESP_LOGD(TAG, "Water temperature=%.1f", this->water_temp_);
-   this->publish_state();
+      case 5:
+        //ESP_LOGD(TAG, "Season=%.1f", value);
+        this->season_ = value;   
+      break;
+      case 6:
+        value /= 10.0;
+        //ESP_LOGD(TAG, "Water temperature=%.1f", value);
+        this->water_temp_ = value;   
+      break;
+    }
+    if (++this->state_ > 6)
+      this->state_ = 0;
+    ESP_LOGD(TAG, "Air temperature=%.1f", this->current_temp_);
+    ESP_LOGD(TAG, "Setpoint temperature=%.1f", this->target_temp_);
+    ESP_LOGD(TAG, "Fan speed=%.1f", this->fan_speed_);
+    ESP_LOGD(TAG, "Program=%.1f", this->program_);
+    ESP_LOGD(TAG, "Season=%.1f", this->season_);
+    ESP_LOGD(TAG, "Water temperature=%.1f", this->water_temp_);
+    this->publish_state();
 }
 
 void Innova::loop() {
-  uint32_t now = millis();
-   // timeout after 15 seconds
-  if (this->waiting_ && (now - this->last_send_ > 15000)) {
-    ESP_LOGW(TAG, "timed out waiting for response");
-    this->waiting_ = false;
-  }
-  if (this->waiting_ || (this->state_ == 0))
-    return;
-  this->last_send_ = now;
-  send(CMD_READ_REG, REGISTER[this->state_ - 1], 1);
-  this->waiting_ = true;
+    uint32_t now = millis();
+    // timeout after 15 seconds
+    if (this->waiting_ && (now - this->last_send_ > 15000)) {
+      ESP_LOGW(TAG, "timed out waiting for response");
+      this->waiting_ = false;
+    }
+    if (this->waiting_ || (this->state_ == 0))
+      return;
+    this->last_send_ = now;
+    send(CMD_READ_REG, REGISTER[this->state_ - 1], 1);
+    this->waiting_ = true;
 }
 
 void Innova::update() {
-   this->state_ = 1;
+    this->state_ = 1;
 }
 
 void Innova::control(const climate::ClimateCall &call) {
 
-    if (call.get_mode().has_value()) 
-    {
-        int curr_prg = (int) this->program_;
-	int new_prg = curr_prg;	    
-        this->mode = *call.get_mode();    
-        climate::ClimateMode mode = *call.get_mode();
-        switch (mode) {
-	    case climate::CLIMATE_MODE_OFF:
-                //ESP_LOGD(TAG, "Set Climate Mode: OFF");
+    if (call.get_mode().has_value()) {
+      int curr_prg = (int) this->program_;
+	    int new_prg = curr_prg;	    
+      this->mode = *call.get_mode();    
+      climate::ClimateMode mode = *call.get_mode();
+      switch (mode) {
+	      case climate::CLIMATE_MODE_OFF:
+          //ESP_LOGD(TAG, "Set Climate Mode: OFF");
 	        //write_register((new_prg | (1 << 7)), INNOVA_PROGRAM);
-		new_prg = curr_prg | (1 << 7);
-	    break;
-	    case climate::CLIMATE_MODE_HEAT:
+		      new_prg = curr_prg | (1 << 7);
+	      break;
+	      case climate::CLIMATE_MODE_HEAT:
 	        //ESP_LOGD(TAG, "Set Climate Mode: HEAT");
 	        write_register(3, INNOVA_SEASON);
 	        //write_register((new_prg & ~(1 << 7)), INNOVA_PROGRAM);
-		new_prg = curr_prg & ~(1 << 7);    
-	    break;
-	    case climate::CLIMATE_MODE_COOL:
+		      new_prg = curr_prg & ~(1 << 7);    
+	      break;
+	      case climate::CLIMATE_MODE_COOL:
 	        //ESP_LOGD(TAG, "Set Climate Mode:COOL");
 	        write_register(5, INNOVA_SEASON);
 	        //write_register((new_prg & ~(1 << 7)), INNOVA_PROGRAM);
-		new_prg = curr_prg & ~(1 << 7);     
-	     break;
-	     default: 
-		ESP_LOGW(TAG, "Unsupported mode: %d", mode); 
-	    break;
-        }
-	  ESP_LOGD(TAG, "Climate mode set to: %i", mode);	    
-	  write_register(new_prg, INNOVA_PROGRAM);
-	    
+		      new_prg = curr_prg & ~(1 << 7);     
+	      break;
+	      default: 
+		      ESP_LOGW(TAG, "Unsupported mode: %d", mode); 
+	      break;
+      }
+	    ESP_LOGD(TAG, "Climate mode set to: %i", mode);	    
+	    write_register(new_prg, INNOVA_PROGRAM);
     }
 
-    if (call.get_fan_mode().has_value()) 
-    {
-	  int mode;
-	  this->fan_mode = *call.get_fan_mode();
-	  switch (fan_mode.value()) {
-		  case climate::CLIMATE_FAN_LOW: mode = 2; break;
-		  case climate::CLIMATE_FAN_MEDIUM: mode = 1; break;
-		  case climate::CLIMATE_FAN_HIGH: mode = 3; break;
-		  case climate::CLIMATE_FAN_AUTO: mode = 0; break;
-		  default: mode = 2; break;
-	  }
-	  ESP_LOGD(TAG, "Fan mode set to: %i", mode);
-	  write_register(mode, INNOVA_PROGRAM);
+    if (call.get_fan_mode().has_value()) {
+	    int mode;
+	    this->fan_mode = *call.get_fan_mode();
+	    switch (fan_mode.value()) {
+		    case climate::CLIMATE_FAN_LOW: mode = 2; break;
+		    case climate::CLIMATE_FAN_MEDIUM: mode = 1; break;
+		    case climate::CLIMATE_FAN_HIGH: mode = 3; break;
+		    case climate::CLIMATE_FAN_AUTO: mode = 0; break;
+		    default: mode = 2; break;
+	    }
+	    ESP_LOGD(TAG, "Fan mode set to: %i", mode);
+	    write_register(mode, INNOVA_PROGRAM);
     }
 	
     if (call.get_target_temperature().has_value()) {
@@ -170,7 +167,7 @@ void Innova::control(const climate::ClimateCall &call) {
       write_register(target, INNOVA_SETPOINT);
     }
     //this->publish_state();
-  }
+}
 
 void Innova::write_register(float new_value, uint16_t address)
 {
@@ -178,19 +175,18 @@ void Innova::write_register(float new_value, uint16_t address)
       uint8_t payload[] = {(uint8_t)(value_to_write >> 8), (uint8_t)value_to_write };
       waiting_for_write_ack_= true;
       send(CMD_WRITE_REG,address,1,sizeof(payload),payload);
-
 }
 
 void Innova::dump_config() { 
   LOG_CLIMATE("", "Innova Climate", this); 
   ESP_LOGCONFIG(TAG, "INNOVA:");
   ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
-//  ESP_LOGD(TAG, "Air temperature=%.1f", this->current_temp_);
-//  ESP_LOGD(TAG, "Setpoint temperature=%.1f", this->target_temp_);
-//  ESP_LOGD(TAG, "Fan speed=%.1f", this->fan_speed_);
-//  ESP_LOGD(TAG, "Program=%.1f", this->program_);
-//  ESP_LOGD(TAG, "Season=%.1f", this->season_);
-//  ESP_LOGD(TAG, "Water temperature=%.1f", this->water_temp_);
+  //ESP_LOGD(TAG, "Air temperature=%.1f", this->current_temp_);
+  //ESP_LOGD(TAG, "Setpoint temperature=%.1f", this->target_temp_);
+  //ESP_LOGD(TAG, "Fan speed=%.1f", this->fan_speed_);
+  //ESP_LOGD(TAG, "Program=%.1f", this->program_);
+  //ESP_LOGD(TAG, "Season=%.1f", this->season_);
+  //ESP_LOGD(TAG, "Water temperature=%.1f", this->water_temp_);
 }
 
 }  // namespace innova
