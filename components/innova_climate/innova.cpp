9b55818a17494bbaf7d16efe415525b0be7f3bca
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-16 16:48:22 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -151,19 +151,18 @@ void Innova::control(const climate::ClimateCall &call) {
 	        ESP_LOGD(TAG, "Set Climate Mode: HEAT");
 	        write_register(3, INNOVA_SEASON);
 		new_prg = curr_prg & ~(1 << 7);  
-		//write_register(new_prg, INNOVA_PROGRAM);
+		write_register(new_prg, INNOVA_PROGRAM);
 	      break;
 	      case climate::CLIMATE_MODE_COOL:
 	        ESP_LOGD(TAG, "Set Climate Mode:COOL");
 	        write_register(5, INNOVA_SEASON);
 		new_prg = curr_prg & ~(1 << 7);
-		//write_register(new_prg, INNOVA_PROGRAM);
+		write_register(new_prg, INNOVA_PROGRAM);
 	      break;
 	      default: 
 		      ESP_LOGW(TAG, "Unsupported mode: %d", mode); 
 	      break;
       }	    
-	    //write_register(new_prg, INNOVA_PROGRAM);
     }
 
     if (call.get_fan_mode().has_value()) {
