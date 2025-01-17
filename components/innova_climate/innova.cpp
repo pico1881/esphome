7e77d9dd21d085241663f4552ea5ec1147897e65
pico1881 <82042085+pico1881@users.noreply.github.com>
2025-01-17 15:43:24 +0100

    Update innova.cpp

--- components/innova_climate/innova.cpp
+++ components/innova_climate/innova.cpp
@@ -144,7 +144,7 @@ void Innova::add_to_queue(uint8_t function, float new_value, uint16_t address) {
 
 void Innova::writeModbusRegister(WriteableData write_data) {
     uint8_t payload[] = {(uint8_t)(write_data.write_value >> 8), (uint8_t)write_data.write_value };
-    send( data.function_value,data.register_value,1,sizeof(payload),payload);
+    send( write_data.function_value,write_data.register_value,1,sizeof(payload),payload);
 
     //uint16_t value_to_write = new_value;
     //uint8_t payload[] = {(uint8_t)(value_to_write >> 8), (uint8_t)value_to_write };
